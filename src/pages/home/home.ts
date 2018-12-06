babd302b1c866f1477c2dc75cb46767e946a5d7c
Gerba <gerbenostermann1989@gmail.com>
2018-12-06 22:46:19 +0100

    First commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { NavController, ToastController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  personName: string;
+
+  constructor(public toast: ToastController) {
+
+  }
+
+  showGreeting(name: string) {
+    this.toast.create({
+      message: `Hello ${name}`,
+      duration: 3000
+    }).present();
+
+  }
+}
